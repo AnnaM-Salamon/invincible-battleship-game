b06ff2289694129be02fa56d43b6f6493f62525f
Anna  Salamon <121969790+AnnaM-Salamon@users.noreply.github.com>
2024-06-01 19:08:21 +0000

    Add method position_ship to Board class

--- run.py
+++ run.py
@@ -2,15 +2,12 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from random import randint
-
-
-
-        
+     
 class Board:
     """
-    Main board class. Defines board size 9 rows and columns), the player's name, the number of ships
+    Main board class. Defines board size (rows and columns),
+    the player's name, the number of ships
     """
-    
     def __init__(self, size, name, board, ships):
         self.size = size
         self.name = name
@@ -21,5 +18,13 @@ class Board:
                 row.append('-')
                 self.board.append(row)
             self.ships = []    
+            
+    def position_ship(self, index_row, index_col):
+        """
+        Function to position ships on the player's board marked as 'S'
+        """       
+        self.board[index_row][index_col] = 'S'
+        self.ships.append((index_row, index_col))
+            
             
         
